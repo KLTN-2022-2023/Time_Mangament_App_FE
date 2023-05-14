17087ab08e7b30ac0d5aab5199fc1d68aed75263
Le Nhat Huy <lenhathuy2212000@gmail.com>
2023-05-14 19:50:11 +0700

    fix save user

--- Screen/UserDetailScreen/UserDetailComponent.js
+++ Screen/UserDetailScreen/UserDetailComponent.js
@@ -16,14 +16,19 @@ import {
 } from "native-base";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import { useEffect, useState } from "react";
-import { UpdateProfile, getInfoUser, getUserById } from "../../Reducers/UserReducer";
+import {
+  UpdateProfile,
+  getInfoUser,
+  getUserById,
+  getUser,
+} from "../../Reducers/UserReducer";
 import jwt_decode from "jwt-decode";
 import { useSelector, useDispatch } from "react-redux";
 import { StyleSheet, TouchableOpacity } from "react-native";
 import { TextInput } from "react-native";
 import IconICon from "react-native-vector-icons/Ionicons";
 import IconFontisto from "react-native-vector-icons/Fontisto";
-import * as ImagePicker from 'expo-image-picker';
+import * as ImagePicker from "expo-image-picker";
 import SnackBar from "../../Component/Snackbar/Snackbar";
 
 export default ({ navigation }) => {
@@ -46,7 +51,6 @@ export default ({ navigation }) => {
       setName(data.name);
       setPhone(data.phone);
     }
-
   };
 
   const HandleUpdateProfile = async () => {
@@ -54,35 +58,36 @@ export default ({ navigation }) => {
     try {
       const decoded = jwt_decode(token);
       const userId = decoded._id;
-      const response = await UpdateProfile({ userId, email, name, phone }, token);
-      console.log(response)
+      const response = await UpdateProfile(
+        { userId, email, name, phone },
+        token
+      );
+      console.log(response);
+      dispatch(getUser({ _id: userId, name: name }));
       setSnackBar(true);
-    } catch (err) {
-
-    }
-  }
+    } catch (err) {}
+  };
 
   useEffect(() => {
     HandleGetInfoUser();
   }, []);
 
-
   const style = StyleSheet.create({
     title: {
       fontWeight: 800,
-      fontSize: 18
+      fontSize: 18,
     },
     heading: {
       justifyContent: "space-between",
       alignItems: "center",
-      paddingTop: 20
+      paddingTop: 20,
     },
     buttonSave: {
       color: "#0000FF",
-      fontSize: 20
+      fontSize: 20,
     },
     text: {
-      fontSize: 18
+      fontSize: 18,
     },
     shadow: {
       paddingLeft: 5,
@@ -95,48 +100,98 @@ export default ({ navigation }) => {
         width: 3,
       },
       elevation: 1,
-    }
-  })
+    },
+  });
   return (
     <NativeBaseProvider>
       <Box safeArea p="2" py="2" w="100%" maxW="350" width={500}>
         <View>
           <HStack alignItems={"center"} justifyContent={"space-between"}>
             <HStack alignItems={"center"} backgroundColor={"#FF00000"}>
-              <IconICon size={25}
+              <IconICon
+                size={25}
                 color={"#000000"}
                 name="arrow-back"
-                onPress={() => navigation.goBack()}
+                onPress={() =>
+                  navigation.navigate("HomeTab", {
+                    screen: "Settings",
+                    params: { name: name },
+                  })
+                }
               />
-              <Text color={"#000000"} fontWeight={800} fontSize={20} paddingLeft={5}>Profile</Text>
+              <Text
+                color={"#000000"}
+                fontWeight={800}
+                fontSize={20}
+                paddingLeft={5}
+              >
+                Profile
+              </Text>
             </HStack>
-            {update ? <Text style={style.buttonSave} fontWeight={500} onPress={HandleUpdateProfile}>Save</Text> : null}
+            {update ? (
+              <Text
+                style={style.buttonSave}
+                fontWeight={500}
+                onPress={HandleUpdateProfile}
+              >
+                Save
+              </Text>
+            ) : null}
           </HStack>
           <View backgroundColor={"white"} marginTop={5} alignItems={"center"}>
-            <Avatar size={"2xl"} bg="green.500" source={{
-              uri: "https://static.vecteezy.com/system/resources/previews/000/439/863/original/vector-users-icon.jpg"
-            }}>
+            <Avatar
+              size={"2xl"}
+              bg="green.500"
+              source={{
+                uri: "https://static.vecteezy.com/system/resources/previews/000/439/863/original/vector-users-icon.jpg",
+              }}
+            >
               AJ
             </Avatar>
             <View>
-              <HStack alignItems="center" marginTop={5} height={10} >
+              <HStack alignItems="center" marginTop={5} height={10}>
                 <IconFontisto name="email" size={20} color={"#000000"} />
-                <TextInput paddingLeft={20} style={style.text} value={email} onChangeText={(e) => { setEmail(e), setUpdate(true) }} />
+                <TextInput
+                  paddingLeft={20}
+                  style={style.text}
+                  value={email}
+                  onChangeText={(e) => {
+                    setEmail(e), setUpdate(true);
+                  }}
+                />
               </HStack>
-              <HStack alignItems="center" marginTop={5} height={10} >
+              <HStack alignItems="center" marginTop={5} height={10}>
                 <IconFontisto name="male" size={20} color={"#000000"} />
-                <TextInput paddingLeft={20} style={style.text} value={name} onChangeText={(e) => { setName(e), setUpdate(true) }} />
+                <TextInput
+                  paddingLeft={20}
+                  style={style.text}
+                  value={name}
+                  onChangeText={(e) => {
+                    setName(e), setUpdate(true);
+                  }}
+                />
               </HStack>
               <HStack alignItems="center" marginTop={5} height={10}>
                 <IconICon name="call-outline" size={20} color={"#000000"} />
-                <TextInput paddingLeft={20} style={style.text} value={phone} editable={false} onChangeText={(e) => setPhone(e)} />
+                <TextInput
+                  paddingLeft={20}
+                  style={style.text}
+                  value={phone}
+                  editable={false}
+                  onChangeText={(e) => setPhone(e)}
+                />
               </HStack>
             </View>
           </View>
         </View>
       </Box>
-      {snackbar ? <SnackBar label={"Thay đổi thông tin thành công"} backgroundColor={{ backgroundColor: "green" }} onPress={() => setSnackBar(false)} /> : null}
+      {snackbar ? (
+        <SnackBar
+          label={"Thay đổi thông tin thành công"}
+          backgroundColor={{ backgroundColor: "green" }}
+          onPress={() => setSnackBar(false)}
+        />
+      ) : null}
     </NativeBaseProvider>
-
   );
 };
